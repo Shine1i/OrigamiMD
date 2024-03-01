bd78cd97fc5f008073113549d023914bcb31e5a8
grovre <landonvee111@gmail.com>
2024-03-01 15:48:49 -0600

    service ctrlr, backend request model

---
+++ GrazieBackend/Models/BackendRequestType.cs
@@ -0,0 +1,15 @@
+﻿namespace GrazieBackend.Models;
+
+public enum BackendRequestType
+{
+    AdjustTone,
+    Complete,
+    DeEmojify,
+    Extnd,
+    FixGrammar,
+    Prompt,
+    Rephrase,
+    Summarize,
+    Eli5,
+    Translate
+}
