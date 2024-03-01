d06f64c6abd67d541ff72770ed863a643456885e
grovre <landonvee111@gmail.com>
2024-03-01 16:19:00 -0600

    Grazie auth key provider

--- GrazieBackend/Services/GrazieService.cs
+++ GrazieBackend/Services/GrazieService.cs
@@ -1,7 +1,9 @@
 ﻿namespace GrazieBackend.Services;
 
-public class GrazieService : IAiService
+public class GrazieService(GrazieAuthKeyProvider keyProvider) : IAiService
 {
+    private string _key = keyProvider.GetKey();
+    
     public string PromptAi(string prompt)
     {
         throw new NotImplementedException();
