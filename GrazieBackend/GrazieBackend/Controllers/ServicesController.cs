d641f9da16fea4f461b8212bc8ea228b05de1d81
YBGE <erdembagciw58@gmail.com>
2024-03-02 22:35:25 +0100

    Accept list of BackendGrazieRequests

--- GrazieBackend/GrazieBackend/Controllers/ServicesController.cs
+++ GrazieBackend/GrazieBackend/Controllers/ServicesController.cs
@@ -8,16 +8,22 @@ namespace GrazieBackend.Controllers;
 [ApiController]
 public class ServicesController(GrazieService grazieService, GrazieRequestTypeToGrazieMessageStrategy converterStrategy) : ControllerBase
 {
-    // TODO: accept a list of BackendGrazieRequests with the first one being the sys msg and the rest being additional messages for configuration
+    // TODO: accept a list of BackendGrazieRequests with the first one being the sys msg and the rest being additional messages for configuration <|> AWAITING TEST
     [HttpPost("grazie")]
     public async Task<ActionResult> PromptGrazie(BackendGrazieRequest request)
     {
         // TODO: Catch NotImplementedException if strategy conversion fails from bad request type
         var sysMsg = converterStrategy.Convert(request);
-        var userMsg = new GrazieMessage(GrazieMessage.UserMessage, request.Prompt);
-        var messages = new[] { sysMsg, userMsg };
+        List<GrazieMessage> userMsg = new List<GrazieMessage>();
+        foreach (var item in request.Prompt)
+        {
+            userMsg.Add(new GrazieMessage(GrazieMessage.UserMessage, item));
+        }
+
+        //var messages = new[] { sysMsg, userMsg[0] }; FUCK THIS, it's dogshit
+        userMsg.Insert(0, sysMsg);
         
-        var response = await grazieService.PromptAi(messages);
+        var response = await grazieService.PromptAi(userMsg);
 
         var result = new ContentResult
         {
